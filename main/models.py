21dd985117b0c423bd52663795f4689b4a08f1d4
ndnrys <5791typezero0828@gmail.com>
2023-03-11 18:06:26 +0900

    models作成途中とテーブル構成作成

--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    name = models.CharField(max_length=256)
+    password = models.CharField(max_length=256)
+    department_id = models.SmallIntegerField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
