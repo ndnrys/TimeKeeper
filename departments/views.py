ec8b599f710216f8988445354f00a98dd46eaabd
ndnrys <5791typezero0828@gmail.com>
2023-03-21 18:06:05 +0900

    部署一覧を作成

--- departments/views.py
+++ departments/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from app.models import Departments
 
 def list(request):
-    return render(request, 'app/departments/list.html', {'name': '部署管理'})
+    context = {
+        'departments_list': Departments.objects.all(),
+    }
+    return render(request, 'app/departments/list.html', context)
